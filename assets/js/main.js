267b382a83b25baf3ec6970a8194f0a25f17350a
lampronmuehleisenvt571-dev <lampronmuehleisenvt571@gmail.com>
2026-06-17 03:26:43 +0630

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+```
+console.log("TM Casino Loaded Successfully");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        console.log(button.innerText + " clicked");
+    });
+});
+
+const forms = document.querySelectorAll("form");
+
+forms.forEach(form => {
+    form.addEventListener("submit", function () {
+        console.log("Form Submitted");
+    });
+});
+```
+
+});
